8a3efae1e9840bfc58677c9ad7b502670f8b5988
Harris Carney <harriscarney@outlook.com>
2015-10-24 09:41:37 -0500

    General advancements

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+/*
+* First click - generate HTML. 
+* On close move HTML above page and disable.
+* All other clicks - move already generated HTML.
+*/
+
+var firstUse = true;
+var isActive = false;
+
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.executeScript(null, {file: 'contentScript.js' });
+  if(firstUse) {
+    chrome.tabs.insertCSS(null, { file: 'dropdown.css' });
+    chrome.tabs.executeScript(null, { file: 'jquery-2.1.4.min.js' });
+    chrome.tabs.executeScript(null, { file: 'oauth.js' });
+    chrome.tabs.executeScript(null, { file: 'generate.js' });
+  }
+  
+  chrome.tabs.executeScript(null, { 
+    code: 
+  });
 });
--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,23 @@
-var isOpened = false;
+$('body').append('<div id="cpContainer"></div>');
 
-var CodePanion = document.createElement('div');
-CodePanion.setAttribute('id', 'CodePanion');
-CodePanion.innerHtml = '<object type="text/html" data="/dropdown.html"></object>';
+var gh_username, gh_pas, gh_url, gh_path, gh_path_len, gh_commit_msg, gh_path_pieces, gh_data;
 
-document.body.insertBefore(CodePanion, document.body.childNodes[0]);
+OAuth.initialize('qg3uN6ehTJYw_uXad1AO8iki2WA');
 
+githubLogin.onclick = function() {
+  console.log('check');
+      OAuth.popup('github', {cache: true})
+        .done(function(result){
+          console.log(result.access_token);
+          gh_pas = result.access_token;
+          
+          result.me().done(function(data){
+              console.log(data.alias);
+              gh_username = data.alias;
+          });
+      
+      })
+        .fail(function(err){
+          console.log(err)
+      });
+}
