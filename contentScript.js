8ee782732353fb3ea43676912b51eac6733d40f9
Harris Carney <harriscarney@outlook.com>
2015-10-24 02:37:47 -0500

    Initial Commit.

---
+++ contentScript.js
@@ -0,0 +1,8 @@
+var isOpened = false;
+
+var CodePanion = document.createElement('div');
+CodePanion.setAttribute('id', 'CodePanion');
+CodePanion.innerHtml = '<object type="text/html" data="/dropdown.html"></object>';
+
+document.body.insertBefore(CodePanion, document.body.childNodes[0]);
+
